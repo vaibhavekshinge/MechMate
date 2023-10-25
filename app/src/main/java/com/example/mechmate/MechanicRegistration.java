cd49722aa6c4f78edbf354633ba0bcad6fc09ee8
Sanket <sankya0444312@gmail.com>
2023-10-25 23:29:06 +0530

    added mec regis page

---
+++ app/src/main/java/com/example/mechmate/MechanicRegistration.java
@@ -0,0 +1,14 @@
+package com.example.mechmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MechanicRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mechanic_registration);
+    }
+}
