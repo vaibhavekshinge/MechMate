d4ef6bfb3fd8ad38d5e2a2034122a5ba093a2085
Shreyas <shreyas.raijade@gmail.com>
2023-10-30 00:23:03 +0530

    buy accesories page update

---
+++ app/src/main/java/com/example/mechmate/BuyAccessories.java
@@ -0,0 +1,31 @@
+package com.example.mechmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class BuyAccessories extends AppCompatActivity {
+    ImageView backacces;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_accessories);
+        backacces = findViewById(R.id.backacces);
+
+        backacces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intent);
+
+                }
+
+            });
+        }
+    }
